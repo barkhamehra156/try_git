b051e36ea0c34ae97c1803f4047c009507a4b12b
barkha <barkha@barkha-ThinkPad-Edge-E430.(none)>
2014-11-23 11:39:40 +0530

    Adding new tables

--- TestCase/case1/admin.py
+++ TestCase/case1/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from case1.models import TestEnvironment, Story, TestCase, TestSuiteType, TestSuite
+from case1.models import TestEnvironment, Story, TestCase, TestSuiteType, TestSuite, TestCaseSuite, TestPlan , TestPlanMap, TestStatus
 
 class StoryAdmin(admin.ModelAdmin):
 	list_display = ('story_name', 'description')
+
+class TestCaseSuiteAdmin(admin.ModelAdmin):
+	list_display = ('TestCaseId', 'TestSuiteId')
 
 admin.site.register(TestCase)
 admin.site.register(Story,StoryAdmin)
 admin.site.register(TestSuiteType)
 admin.site.register(TestSuite)
 admin.site.register(TestEnvironment)
+admin.site.register(TestCaseSuite)
+admin.site.register(TestPlan)
+admin.site.register(TestPlanMap)
+admin.site.register(TestStatus)
 # Register your models here.
--- TestCase/case1/models.py
+++ TestCase/case1/models.py
@@ -18,12 +18,12 @@ class Story(models.Model):
 	
 class TestCase(models.Model):
 
-	priority_choices = [(i,i) for i in range(5)]
-	category_choices = (('Smoke','smoke'),('Core','core'))
+	priority_choice_list = [(i,i) for i in range(5)]
+	category_choice_list = (('Smoke','smoke'),('Core','core'))
 	story_id = models.ForeignKey(Story, db_column='StoryId')
 	description_text = models.TextField(db_column = 'Description')
-	category_choices = models.CharField(max_length = 25, db_column='Category',choices = category_choices)
-	priority_choices = models.IntegerField(max_length = 2, db_column='Priority',choices = priority_choices)
+	category_choices = models.CharField(max_length = 25, db_column='Category',choices = category_choice_list)
+	priority_choices = models.IntegerField(max_length = 2, db_column='Priority',choices = priority_choice_list)
 	precondition_text = models.TextField(db_column = 'Pre-Condition')
 	summary_text = models.TextField(db_column = 'Summary')
 	teststep_text = models.TextField(db_column = 'Test Steps')
@@ -40,5 +40,24 @@ class TestSuite(models.Model):
 	def __unicode__(self):
 		return unicode(self.testsuite_name)
 	testsuite_name = models.CharField(max_length = '100', db_column = 'Name')
-	testcase_id = models.ForeignKey(TestSuiteType, db_column = 'TestCaseId', related_name = '+')
 	testsuitetype_id = models.ForeignKey(TestSuiteType, db_column = 'TestSuiteTypeId')
+
+
+class TestCaseSuite(models.Model):
+	testcase_id = models.ForeignKey(TestCase, db_column = 'TestCaseId', related_name = '+')
+	testsuite_id = models.ForeignKey(TestSuite, db_column='TestSuiteId')
+
+class TestPlan(models.Model):
+	testplan_name =  models.CharField(max_length = '100', db_column = 'Name')
+	start_time = models.DateTimeField(null=True, db_column='StartTime', blank=True)
+	end_time = models.DateTimeField(null=True, db_column='EndTime', blank=True)
+
+class TestStatus(models.Model):
+	status = models.CharField(max_length = 100, db_column = 'Status')
+
+class TestPlanMap(models.Model):
+	testPlan_id = models.ForeignKey(TestPlan, db_column='testPlanId')
+	testcase_id = models.ForeignKey(TestCase, db_column = 'TestCaseId', related_name = '+')
+	testsuite_id = models.ForeignKey(TestSuite, db_column = 'TestSuiteId', related_name = '+')
+	status = models.ForeignKey(TestStatus, db_column = 'Status')
+	remarks = models.TextField(db_column='Remarks')
